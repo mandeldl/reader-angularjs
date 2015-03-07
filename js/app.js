82f255a15dd536e6ef26f22f35d7c5ad98ad561e
David Mandel <MandelDL@gmail.com>
2015-03-07 16:27:48 -0500

    create route for bookId

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ app.config(function($routeProvider) {
 		controller: 'BookshelfController',
 		templateUrl: 'views/bookshelf.html'
 	})
+	.when('books/:bookId', {
+		controller: 'BookController',
+		templateUrl: 'book.html'
+	})
 	.otherwise({
 		redirectTo: '/books'
 	})
