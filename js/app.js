3a8acc52e9a77d2cee14e178653792f9709f838a
David Mandel <MandelDL@gmail.com>
2015-03-07 15:58:34 -0500

    create route for /books

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 var app = angular.module('ReaderApp', ['ngRoute']);
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/books', {
+		controller: 'BookshelfController',
+		templateUrl: 'views/bookshelf.html'
+	})
+	.otherwise({
+		redirectTo: '/books'
+	})
+});
