2ad151d6b85e974d0910fb61f0111e218ae9926c
David Mandel <MandelDL@gmail.com>
2015-03-09 14:20:47 -0400

    use $scope.book.chapters[routeparams] to create $scope.chapter

--- js/controllers/ChapterController.js
+++ js/controllers/ChapterController.js
@@ -2,7 +2,7 @@ app.controller('ChapterController', ['$scope', 'books', '$routeParams', function
   books.success(function(data) {
     // Your code here
     $scope.book = data[$routeParams.bookId];
-    $scope.chapter = book.chapters[$routeParams.chapterId];
+    $scope.chapter = $scope.book.chapters[$routeParams.chapterId];
 
     // If there no more chapters left, go back to the bookshelf view
     if($routeParams.chapterId >= $scope.book.chapters.length - 1) {
